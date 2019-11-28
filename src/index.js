bf995e85300cf282ef65f0c3667d542ff2e33a56
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2019-11-28 11:04:34 +0100

    Fix tags overflow style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import './styles/Chip.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
