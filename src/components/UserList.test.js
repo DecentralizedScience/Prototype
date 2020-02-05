e77c4ab82a1d31b0c3401ea6a6933808b449ce50
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2020-02-05 17:59:53 +0100

    Add UserLists tests

---
+++ src/components/UserList.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import UserList from './UserList';
+import { MockedProvider } from '@apollo/react-testing';
+import '@testing-library/jest-dom/extend-expect'
+import {  } from 'react-dom/test-utils';
+
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+import {USERS_QUERY} from './UserList';
+import testData from './UserList.testdata.json';
+
+const waitForExpect = require("wait-for-expect")
+
+waitForExpect.defaults.timeout = 10000;
+
+const mocks = [
+  {
+    request: {
+      query: USERS_QUERY
+    },
+    result: {
+      data:  testData.data
+    }
+  }
+];
+
+test('UserList shows first user\'s name', async () => {
+
+    const { getByText } = render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <UserList />
+    </MockedProvider>)
+
+  await waitForElement(() => getByText(mocks[0].result.data.users[0].name, {exact: false}))
+
+}, 12000
+);
