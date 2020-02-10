a8181f0d9ba89c73881a1635af416ad354b31056
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2020-02-10 15:15:19 +0100

    Add e2e tests to check unnocupied filter behaviour

--- cypress/integration/searchInput.spec.js
+++ cypress/integration/searchInput.spec.js
@@ -1,9 +1,31 @@
 describe("Search input", () => {
+
   beforeEach( ()=>{
+    // visit main page
     cy.visit("/")
+    // wait for an element on the reviewers table
+    cy.get('.MuiTableCell-body')
   })
 
   it("Autofocus on page load", () => {
     cy.get('[aria-label="reviewers search"]').focus()
   })
+
+  it("Allows click in the 'only unoccupied' filter", () => {
+    cy.get('[aria-label="primary checkbox"]')
+    .click()
+    cy.get('[aria-label="primary checkbox"]')
+    .check()
+  })
+
+  it("Filters 'Occupied users' when clicking the filter", () => {
+
+    cy.get('[aria-label="primary checkbox"]')
+    .click()
+    // All badges should all have green color (unnocupied users)
+    cy.get('.MuiBadge-badge').each((el) => {
+      cy.wrap(el).should('have.css', 'background-color', 'rgb(76, 175, 80)')
+    })
+  })
+
 })
