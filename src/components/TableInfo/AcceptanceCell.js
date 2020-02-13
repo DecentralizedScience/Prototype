a5d3cf4a7ea5284ef5718709b0104b1d5a828eaf
ElenaPT <elenapt95@gmail.com>
2020-02-13 10:24:15 +0100

    Warning fix

--- src/components/TableInfo/AcceptanceCell.js
+++ src/components/TableInfo/AcceptanceCell.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Tooltip from '@material-ui/core/Tooltip'
 import Button from '@material-ui/core/Button'
-import Icon from '@material-ui/core/Icon'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined'
